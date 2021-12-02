604c17fd81bca1f16562e70a9eb3353508ce436f
Adam Wales <adam.wales1@outlook.com>
2021-12-01 22:16:04 -0800

    test

--- Test1.ps1
+++ Test1.ps1
@@ -1,1 +1,15 @@
-Get-Service -Name Spooler
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
